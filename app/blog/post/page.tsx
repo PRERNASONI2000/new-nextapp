38d09622387fddc740b91e82e3378f5096242282
PRERNASONI2000 <167075251+PRERNASONI2000@users.noreply.github.com>
2025-12-27 13:51:07 +0530

    update

---
+++ app/blog/post/page.tsx
@@ -0,0 +1,7 @@
+export default function Post() {
+    return(
+          <div className="bg-pink-200 text-black font-semibold text-[24px] text-center uppercase min-h-screen flex items-center justify-center">
+            my First post
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
       <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     
         <h1 className="text-shadow-black font-bold text-2xl text-center">hello i love nextjs</h1>
-    
+       <Link href="/about">About</Link>
+       <Link href="/contact">Contact</Link>
+       <br />
+       <Link href="/blog/post">See Post</Link>
+       
     </div>
   );
 }
